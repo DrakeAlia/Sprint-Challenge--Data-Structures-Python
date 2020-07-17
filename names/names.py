dc6da2b9a2415e17b5e363f2f2b30a7b5488b36a
Drake Alia <DrakeTesla@macbook-pro.lan>
2020-07-17 10:19:54 -0700

    updated format

--- names/names.py
+++ names/names.py
@@ -59,7 +59,7 @@ names_search_tree = NamesBST(names_1[0])
 for name in names_1:
     names_search_tree.insert(name)
 
-# Runtime Complexity 0(log n)
+# Runtime complexity 0(log n)
 for name in names_2:
     if names_search_tree.contains(name):
         duplicates.append(name)
@@ -69,7 +69,7 @@ end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
 
-# ---------- Stretch Goal -----------
+# ---------- Stretch Goal ---------------------------------------------------------------------
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
