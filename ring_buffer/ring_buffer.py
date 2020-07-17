5c828c7ba0d51dcb5443fbbfd11986cbf8f5db10
Drake Alia <DrakeTesla@macbook-pro.lan>
2020-07-17 09:29:09 -0700

    finished ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,27 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.storage = []
+        self.size = 0
+        self.oldest = 0
 
     def append(self, item):
-        pass
+#  size += 1
+        self.size += 1
 
+# If the ring buffer isn't empty, add to the end of the list
+        if self.size <= self.capacity:
+            self.storage.append(item)
+
+# If ring is full, replace the item at the oldest index
+        else:
+            if self.oldest < self.capacity - 1:
+                self.storage[self.oldest] = item
+                self.oldest += 1
+            else:
+                self.storage[self.oldest] = item
+                self.oldest = 0
+                
+#  Returning storage for capacity 
     def get(self):
-        pass
+        return self.storage
